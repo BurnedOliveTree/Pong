0f5c725912eb3723bc25fffff714b503f4532bfa
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2022-05-12 14:13:07 +0200

    added object collission, score, finishing basing pong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from turtle import back
 import pygame
-from objects import Ball, Paddle
+from objects import Ball, Paddle, TextLabel
 
 
 class Game:
@@ -9,22 +8,35 @@ class Game:
         self.clock = pygame.time.Clock()
         self.fps = 60
         self.background_colour = (26, 26, 64)
-        self.ball = Ball((32, 32), (64, 64))
-        self.paddleOne = Paddle((10, 100), (10, 10))
-        self.paddleTwo = Paddle((10, 100), (size[0] - 20, 10))
+        self.ball = Ball(self.display, (32, 32), (64, 64))
+        self.paddleOne = Paddle(self.display, (20, 100), (0, 0))
+        self.paddleTwo = Paddle(self.display, (20, 100), (size[0] - 20, 0))
+        self.score = (0, 0)
+        self.score_label = TextLabel(self.display, str(self.score[0])+" : "+str(self.score[1]))
         pygame.display.set_caption("Pong")
 
     def render(self):
         self.display.fill(self.background_colour)
-        self.ball.draw(self.display)
-        self.paddleOne.draw(self.display)
-        self.paddleTwo.draw(self.display)
+        self.score_label.draw()
+        self.ball.draw()
+        self.paddleOne.draw()
+        self.paddleTwo.draw()
         pygame.display.update()
         self.clock.tick(self.fps)
 
+    def set_score(self, score: int):
+        if score == 0:
+            return
+        if score < 0:
+            self.score = (self.score[0], self.score[1] + 1)
+        if score > 0:
+            self.score = (self.score[0] + 1, self.score[1])
+        self.score_label.set_text(str(self.score[0])+" : "+str(self.score[1]))
+
     def run(self):
         shouldContinue = True
-        self.ball.set_speed((1, 1))
+        self.ball.set_speed((4, 4))
+        paddleSpeed = 5
 
         while shouldContinue:
             for event in pygame.event.get():
@@ -33,15 +45,15 @@ class Game:
 
             keys = pygame.key.get_pressed()
             if keys[pygame.K_UP]:
-                self.paddleOne.move(self.display, -5)
+                self.paddleTwo.move(-paddleSpeed)
             if keys[pygame.K_DOWN]:
-                self.paddleOne.move(self.display, 5)
+                self.paddleTwo.move(paddleSpeed)
             if keys[pygame.K_w]:
-                self.paddleTwo.move(self.display, -5)
+                self.paddleOne.move(-paddleSpeed)
             if keys[pygame.K_s]:
-                self.paddleTwo.move(self.display, 5)
+                self.paddleOne.move(paddleSpeed)
 
-            self.ball.move(self.display)
+            self.set_score(self.ball.move([self.paddleOne, self.paddleTwo]))
             self.render()
 
 
--- objects.py
+++ objects.py
@@ -2,40 +2,64 @@ import pygame
 
 
 class Drawable:
-    def __init__(self, size: tuple[int, int], position: tuple[int, int], colour: tuple[int, int, int] = (122, 11, 192)):
+    def __init__(self, window: pygame.Surface, size: tuple[int, int], position: tuple[int, int], colour: tuple[int, int, int] = (122, 11, 192)):
+        self.window = window
         self.size = size
         self.colour = colour
         self.surface = pygame.Surface(size, pygame.SRCALPHA)
         self.rect = self.surface.get_rect(x=position[0], y=position[1])
 
-    def draw(self, surface: pygame.Surface):
-        surface.blit(self.surface, self.rect)
+    def draw(self):
+        self.window.blit(self.surface, self.rect)
+
+
+class Paddle(Drawable):
+    def __init__(self, window: pygame.Surface, size: tuple[int, int], position: tuple[int, int], colour: tuple[int, int, int] = (250, 88, 182)):
+        super().__init__(window, size, position, colour)
+        self.surface.fill(colour)
+
+    def move(self, speed: int):
+        self.rect.y += speed
+        self.rect.y = min(max(self.rect.y, 0), self.window.get_height() - self.size[1])
 
 
 class Ball(Drawable):
-    def __init__(self, size: tuple[int, int], position: tuple[int, int], colour: tuple[int, int, int] = (122, 11, 192)):
-        super().__init__(size, position, colour)
+    def __init__(self, window: pygame.Surface, size: tuple[int, int], position: tuple[int, int], colour: tuple[int, int, int] = (250, 88, 182)):
+        super().__init__(window, size, position, colour)
         pygame.draw.ellipse(self.surface, self.colour, [0, 0, self.size[0], self.size[1]])
         self.speed = (0, 0)
 
     def set_speed(self, speed: tuple[int, int]):
         self.speed = speed
 
-    def move(self, surface: pygame.Surface):
+    def move(self, paddles: list[Paddle]) -> int:
         self.rect.x += self.speed[0]
         self.rect.y += self.speed[1]
 
-        if self.rect.x not in range(0, (surface.get_width() - self.size[0])):
+        if self.rect.x not in range(0, (self.window.get_width() - self.size[0])):
             self.speed = (-self.speed[0], self.speed[1])
-        if self.rect.y not in range(0, (surface.get_height() - self.size[1])):
+            if self.rect.x <= 0:
+                return -1
+            if self.rect.x >= (self.window.get_width() - self.size[0]):
+                return 1
+        if self.rect.y not in range(0, (self.window.get_height() - self.size[1])):
             self.speed = (self.speed[0], -self.speed[1])
+        for paddle in paddles:
+            if self.rect.colliderect(paddle.rect):
+                self.speed = (-self.speed[0], self.speed[1])
+        return 0
 
 
-class Paddle(Drawable):
-    def __init__(self, size: tuple[int, int], position: tuple[int, int], colour: tuple[int, int, int] = (122, 11, 192)):
-        super().__init__(size, position, colour)
-        self.surface.fill(colour)
+class TextLabel(Drawable):
+    def __init__(self, window: pygame.Surface, text: str, size: tuple[int, int] = None, position: tuple[int, int] = None, colour: tuple[int, int, int] = (122, 11, 192), font_size: int = 200):
+        super().__init__(window, size if size is not None else (0, 0), position if position is not None else (0, 0), colour)
+        self.text = text
+        self.font = pygame.font.Font(None, font_size)
+        self.surface = self.font.render(self.text, True, self.colour)
+        if position is None:
+            self.rect.x = self.window.get_width() // 2 - self.surface.get_width() // 2
+            self.rect.y = self.window.get_height() // 2 - self.surface.get_height() // 2
 
-    def move(self, surface: pygame.Surface, speed: int):
-        self.rect.y += speed
-        self.rect.y = min(max(self.rect.y, 0), surface.get_height() - self.size[1])
+    def set_text(self, text):
+        self.text = text
+        self.surface = self.font.render(text, True, self.colour)
